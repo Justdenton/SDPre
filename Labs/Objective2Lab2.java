db07bdc664d3de8260366cce5fdaafd9442454a9
“Justin” <jdenton.bills@gmail.com>
2024-06-10 08:51:10 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Justin Denton";
+    int age = 30;
+    String state = "North Carolina";
+    System.out.println("Name: (name) Age: (age) State: (state)");
+  }
+}
