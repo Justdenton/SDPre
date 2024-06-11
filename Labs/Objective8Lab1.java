636e28d8ac60c76640004f67efa6a70d2943ce0a
“Justin” <jdenton.bills@gmail.com>
2024-06-11 13:07:02 -0500

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,16 @@
+// 1st for loop will print numbers 1-10 (increase variable)
+// 2nd for loop will pint numbers 10-1 (decrease variable)
+
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int increaseCounter = 1; increaseCounter <= 10; increaseCounter ++) {
+      System.out.println(increaseCounter);
+    }
+
+    for (int decreaseCounter = 10; decreaseCounter >= 1; decreaseCounter --) {
+      System.out.println(decreaseCounter);
+    }
+
+  }
+
+}
