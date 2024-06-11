c877ff06af7102900bb616f10f6afa9b8c1add6a
“Justin” <jdenton.bills@gmail.com>
2024-06-11 11:46:39 -0500

    complete objective7lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,12 @@
+// Prints numbers in reverse starting with 10 and ends with 1.
+
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+      System.out.println(counter);
+      counter = counter - 1; 
+    }
+  }
+}
