aa4257d5a296a2b26489022097ecf7dec9969498
“Justin” <jdenton.bills@gmail.com>
2024-06-12 15:22:58 -0500

    complete objective9lab5

---
+++ Labs/Objective9Lab5.java
@@ -0,0 +1,105 @@
+// Simulates a calculator that can use 2 numbers to find the sum, average, or tax based on user input.
+
+import java.util.Scanner;
+
+public class Objective9Lab5 {
+
+  public static void main(String[] args) {
+    Scanner kb = new Scanner (System.in);
+		double num1, num2;
+		boolean keepGoing = true;
+		int choice;
+		double answer = 0.0;
+
+		// Prompts user to provide one number amd stores input into num1.
+
+		System.out.println("Please give me a number: ");
+		num1 = kb.nextDouble();
+
+		// Prompts user to provide one (second) number amd stores input into num2.
+
+    System.out.println("Please give me another number: ");
+		num2 = kb.nextDouble();
+
+
+		while (keepGoing) {
+			printMenu();
+			System.out.print("Which would you like to do? ");
+			choice = kb.nextInt();
+
+			// Switch cases call the below methods and prints the result.
+
+			switch (choice) {
+     
+				case 1:
+          answer = findSum(num1, num2);
+          System.out.println(num1 + " + " + num2 + " = " + answer);
+          break;
+        
+        case 2:
+          answer = findAverage(num1, num2);
+          System.out.println("The average of " + num1 + " and " + num2 + " is: " + answer);
+          break;
+        
+        case 3:
+          answer = calcTax(num1, num2);
+          System.out.println("The amount in tax to be collected from a purchase of " + num1 + " and " + num2 + " is: " + answer);
+          break;
+        
+        case 4:
+          System.out.println("You've chosen to quit.");
+          keepGoing = false;
+          break;
+
+        // Default case used just in case user accidentally inputs an invalid number.
+        default:
+          System.out.println("Invalid entry. Please try again.");
+          break;
+
+			}
+
+		}
+
+		kb.close();
+
+	}
+
+	// printMenu method displays the calculator choices.
+	
+	public static void printMenu() {
+		System.out.println();
+		System.out.println("========= MENU =========");
+		System.out.println("|                      |");
+		System.out.println("|   1. Add Numbers     |");
+		System.out.println("|   2. Find Average    |");
+		System.out.println("|   3. Calculate Tax   |");
+		System.out.println("|   4. Exit            |");
+		System.out.println("|                      |");
+		System.out.println("========================");
+		System.out.println();
+	}
+
+	// findSum method returns the sum of 2 doubles.
+
+	public static double findSum(double x, double y) {
+		double sum = x + y;
+		return sum;
+	}
+
+	// findAverage method returns the average of 2 doubles. The doubles are the method's parameters.
+
+	public static double findAverage(double x, double y) {
+		double average = (x + y) / 2;
+		return average;
+	}
+
+  // calcTax method returns a double that represents the amount of tax the user would pay
+  // ONLY the total amount of tax (not the total + tax)
+  // tax rate pf 8.31%
+
+  public static double calcTax(double x, double y) {
+  	double tax = (x + y) * .0831;
+  	return tax;
+  }
+
+}
